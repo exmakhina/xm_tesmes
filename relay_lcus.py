b8b0440053508cfd405db2d7d71eb1ac54fb5f9f
Jérôme Carretero <cJ@zougloub.eu>
2021-03-18 15:18:03 -0400

    relay_lcus: API update

--- relay_lcus.py
+++ relay_lcus.py
@@ -11,22 +11,26 @@ http://www.lctech-inc.com/Hardware/Detail.aspx?id=d8e17734-4209-4e81-aace-aafc3c
 
 """
 
-import time
 import serial
 
 code_open = bytes(bytearray([0xA0, 0x01, 0x01, 0xA2]))
 code_close = bytes(bytearray([0xA0, 0x01, 0x00, 0xA1]))
 
 class Relay_LCUS(object):
-	def __init__(self, port="/dev/ttyUSB0"):
-		ser = serial.Serial()
-		ser.baudrate = 9600
+	def __init__(self, port):
+		self._port = port
+
+	def __enter__(self):
+		port = self._port
+		self._serial = ser = serial.Serial()
 		ser.port = port
+		ser.baudrate = 9600
 		ser.timeout = 0.5
 		ser.open()
-		self._serial = ser
+		return self
 
-	def __del__(self):
+	def __exit__(self, *args):
+		self.set(0)
 		self._serial.close()
 
 	def set(self, value):
@@ -35,9 +39,10 @@ class Relay_LCUS(object):
 
 if __name__ == '__main__':
 
-	s = Relay_LCUS()
-	for idx_loop in range(1):
-		s.set(1)
-		time.sleep(5)
-		s.set(0)
-	
+	import time
+	with Relay_LCUS("/dev/ttyUSB0") as relay:
+		for idx_loop in range(1):
+			s.set(1)
+			time.sleep(5)
+			s.set(0)
+			time.sleep(5)
