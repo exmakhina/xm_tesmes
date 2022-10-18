c33d7768e1de7656556af5bdef16a3f391278221
Jérôme Carretero <cJ@zougloub.eu>
2022-10-18 17:23:53 -0400

    add camera/

---
+++ camera/__main__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 vi:noet
+# SPDX-FileCopyrightText: 2020-2022 Jérôme Carretero <cJ@zougloub.eu> & contributors
+# SPDX-License-Identifier: MIT
+# Capture images w/ a remote-controlled camera
+
